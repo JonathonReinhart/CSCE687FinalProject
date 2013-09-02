8903a682305706656ac1476501b5a7bc78987571
jonathon <jonathon@6fbbc3a5-1c83-4d8f-af09-50d597775295>
2013-09-02 07:32:24 +0000

    [HW] Fixed the flanger, by writing VHDL that XST inferred to be a dual-port block RAM. At FLRATE = 100, the results (on Nothing Else Matters) sound beautiful, but there is a click occasionally. Not sure what the issue is, or if it's the recording, etc.

--- XPS/pcores/audiofx_v1_00_a/hdl/vhdl/dsp_flanger.vhd
+++ XPS/pcores/audiofx_v1_00_a/hdl/vhdl/dsp_flanger.vhd
@@ -17,7 +17,7 @@ end dsp_flanger;
 
 architecture IMP of dsp_flanger is
     --components  
-    component counter is
+    component sawtooth is
         port (
             sys_clk         : in  std_logic;
             samp_ena        : in  std_logic;
@@ -28,46 +28,69 @@ architecture IMP of dsp_flanger is
     end component;
   
     --signals
-    constant MAX_REG        : integer := 450;
-    signal delay_queue      : slv_array(0 to MAX_REG) := (others => (others => '0'));
-    signal delay_line       : std_logic_vector(15 downto 0) := (others => '0');
-    signal rate_line        : std_logic_vector(15 downto 0);
-    signal rate_line2       : natural;
-    signal xd1,xd2          : signed(15 downto 0) := (others => '0');
-    signal f_sel            : std_logic_vector(31 downto 0);
+    constant DELAY_ADDR_BITS    : positive := 10;
+    constant DELAY_RAM_SIZE     : positive := 2**DELAY_ADDR_BITS;
+    constant SAW_MAX            : positive := 480;  -- 10 ms
+    
+    signal delay_ram            : slv_array(0 to DELAY_RAM_SIZE-1) := (others => (others => '0'));
+    signal next_samp_idx        : unsigned(DELAY_ADDR_BITS-1 downto 0);
+    signal delay_samp_idx       : unsigned(DELAY_ADDR_BITS-1 downto 0);
+    
+    signal delay_samp           : std_logic_vector(15 downto 0);
+    
+    signal x_in_div2            : signed(15 downto 0);
+    signal delay_samp_div2      : signed(15 downto 0);
+    
+    signal sawtooth_sig     : std_logic_vector(15 downto 0);
+
     
 begin
 
-    delay_queue(0) <= x_in;
-
-    -- Synchronous delay cycles.
-    DELAYS : process(sys_clk)
-    begin
-        if rising_edge(sys_clk) then
-            if samp_ena = '1' then
-                -- Shift delay_queue by one sample.
-                -- http://stackoverflow.com/questions/18247955
-                delay_queue(1 to delay_queue'high) <= delay_queue(0 to delay_queue'high-1);
-            end if;
-        end if;
-    end process;
-
-    count : counter
+    SAWTOOTH_I : sawtooth
         port map(
             sys_clk => sys_clk,
             samp_ena => samp_ena,
-            top => X"01C0",
+            top => std_logic_vector(to_unsigned(SAW_MAX, 16)), -- TODO: Make this configurable
             rate => rate,
-            dataout => rate_line
+            dataout => sawtooth_sig
         );
-	
-    rate_line2 <= natural(to_integer(unsigned(rate_line)));
+        
 
-    delay_line <= delay_queue(rate_line2);
+    EACH_NEW_SAMPLE : process(sys_clk) is
+    begin
+        if rising_edge(sys_clk) then
+            if samp_ena = '1' then
+            
+                -- Store the next sample and increment the pointer
+            
+                -- Register the next sample
+                delay_ram( to_integer(next_samp_idx) ) <= x_in;
+                
+                -- This will wrap around to zero, creating a circular buffer
+                next_samp_idx <= next_samp_idx + 1;
+                
+                -----------------------------------------------------------------------------------
+                
+                -- Since the RAM is always a power-of-two in length, an underflow in this
+                -- subtraction will cause the desired wrap-around to the end of the RAM.
+                -- (ex. assuming length of 8,  3-5 = -2 = 0b110 = 6 = 8-2)
+                delay_samp_idx <= next_samp_idx - resize(unsigned(sawtooth_sig), DELAY_ADDR_BITS);
 
-    xd1 <= shift_right(signed(x_in),1);
-    xd2 <= shift_right(signed(delay_line),1);
 
-    y_out <= std_logic_vector(xd1+xd2);
+                -- Get the delayed sample from the RAM.
+                delay_samp <= delay_ram( to_integer(delay_samp_idx) );
+                
+            end if;
+        end if;
+    
+    end process EACH_NEW_SAMPLE;
+    
+
+
+    
+    -- Weighted addition (each is 1/2)
+    x_in_div2 <= shift_right(signed(x_in), 1);
+    delay_samp_div2 <= shift_right(signed(delay_samp), 1);
+    y_out <= std_logic_vector( x_in_div2 + delay_samp_div2 );
 
 end IMP;
--- XPS/pcores/audiofx_v1_00_a/hdl/vhdl/dsp_misc.vhd
+++ XPS/pcores/audiofx_v1_00_a/hdl/vhdl/dsp_misc.vhd
@@ -13,49 +13,55 @@ end package data_types;
 ----------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;  --try to use this library as much as possible.
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
-entity counter is
-port (sys_clk : in  std_logic;
-      samp_ena: in  std_logic;
-      top     : in  std_logic_vector(15 downto 0);
-      rate    : in  std_logic_vector(15 downto 0);
-      dataout : out std_logic_vector(15 downto 0)
-      );
-end counter;
+entity sawtooth is
+    port (
+        sys_clk     : in  std_logic;
+        samp_ena    : in  std_logic;
+        top         : in  std_logic_vector(15 downto 0);
+        rate        : in  std_logic_vector(15 downto 0);
+        dataout     : out std_logic_vector(15 downto 0)
+    );
+end sawtooth;
 
-architecture Behavioral of counter is
-signal i   : unsigned(15 downto 0) := (others => '0');
-signal cnt : unsigned(15 downto 0) := (others => '0');
-signal up  : std_logic := '1';
+architecture Behavioral of sawtooth is
+    signal sawval   : unsigned(15 downto 0) := (others => '0');
+    signal cnt      : unsigned(15 downto 0) := (others => '0');
+    signal up       : std_logic := '1';
 begin
 
-process(sys_clk)
-begin
-  --to check the rising edge of the clock signal
-  if(rising_edge(sys_clk)) then  --1
-    if(samp_ena = '1') then
-      dataout <= std_logic_vector(i);
-      cnt <= cnt + 1;
-      if cnt = unsigned(rate) then  --2
-        if (up = '1') then  --3
-          i <= i + X"0001";
-          if(i = unsigned(top)) then  --4
-            up <= '0';
-          end if;  --4
-        else  --3
-          i <= i - X"0001";
-          if(i = X"0001") then  --4
-            up <= '1';
-          end if;  --4
-        end if; --3
-        cnt <= (others => '0');
-      end if;  --2
-    end if;
-  end if; --1
-end process;
+    dataout <= std_logic_vector(sawval);
+
+    process(sys_clk)
+    begin
+        if rising_edge(sys_clk) then
+            if (samp_ena = '1') then
+                cnt <= cnt + 1;
+                
+                -- Once cnt reaches rate, we change our output value.
+                if cnt = unsigned(rate) then
+                    cnt <= (others => '0');
+                    
+                    if (up = '1') then
+                        sawval <= sawval + 1;
+                        if (sawval = unsigned(top)) then
+                            up <= '0';
+                        end if;
+                    else
+                        sawval <= sawval - 1;
+                        if (sawval = 0) then
+                            up <= '1';
+                        end if;
+                    end if;
+                    
+                end if;
+            
+            end if; --samp_ena
+        end if; --rising_edge(sys_clk)
+    end process;
 
 end Behavioral;
 
